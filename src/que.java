8aef09ba401e65716225e146f776e1f824fa4bf1
humphrbp <humphrbp@users.noreply.github.com>
2019-06-20 21:38:13 -0400

    Initial commit

---
+++ src/que.java
@@ -0,0 +1,93 @@
+import java.util.LinkedList;
+
+import java.util.Queue;
+
+import java.util.Scanner;
+
+ 
+
+public class que {
+
+       public static void main(String[] args) {
+
+              Queue<String> alphabet = new LinkedList<>();
+
+ 
+
+              Scanner scan = new Scanner(System.in);
+
+ 
+
+              int Mode = 0;
+
+              String a = "";
+
+              do {
+
+                     System.out.println("Add Letter (1); Remove Letter(2); Display Current Que(3); Quit(4)");
+
+                     Mode = scan.nextInt();
+
+ 
+
+                     if (Mode == 1) {
+
+                           if (alphabet.size() >= 10) {
+
+                                  System.out.println("Sorry.  Que is Full.  Must remove a Letter.");
+
+                           } else {
+
+                                  System.out.println("Add Letters.  Hit Enter to Stop");
+
+                                  do{
+
+                                         a = scan.next();
+
+                                         alphabet.add(a);
+
+                                         }while (alphabet.size() < 10);
+
+                                  System.out.println("Que is now Full.  Must remove a Letter to enter more.");
+
+                           }
+
+                          
+
+                     }
+
+                     if (Mode == 2) {
+
+                           if (alphabet.size() <= 0) {
+
+                                  System.out.println("Sorry.  Que is Empty.  Must add a Letter.");}
+
+                           else {System.out.println("Removed Letter: " + alphabet.remove());}
+
+                     }
+
+                     if (Mode == 3) {
+
+                           // Display contents of the queue.
+
+                           System.out.println("Queue: " + alphabet);
+
+ 
+
+                           int size = alphabet.size();
+
+                           System.out.println("Size of queue: " + size);
+
+                     }
+
+ 
+
+              } while (Mode != 4);
+
+              scan.close();
+
+       }
+
+ 
+
+}
